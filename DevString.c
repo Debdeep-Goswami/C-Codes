cde4d9622cbd24538ac7cd4bb885304f88122270
Debdeep Goswami <53962338+Debdeep-Goswami@users.noreply.github.com>
2020-11-04 19:25:25 +0530

    Add files via upload

---
+++ DevString.c
@@ -0,0 +1,75 @@
+/*******************************************************************
+	Basic String Functions to use in programs by Debdeep
+	
+*******************************************************************/
+	
+
+#include <stdio.h>
+#include <ctype.h>
+#define MAX 1000
+
+
+int readLine(char *string)
+{	
+
+	/***************************************************************
+		Use			:-	To reads a complete line from stdin/console
+		
+		Arguments	:-	The char array where the line will be stored
+		
+		Return 		:-	The length of the line
+		
+	***************************************************************/
+	
+	//	To store the character read from the console
+	char c;						
+	
+	//	To count the number of character read
+	int i=0;					
+
+	//	Main loop to read the characters
+	while(1)
+	{
+		c=getchar();
+		
+		//	Stops as soon as a new line character or EOF is pressed
+		if(c=='\n' || c==EOF)	
+		{
+			//	To mark the end of string
+			string[i]='\0';		
+			break;
+		}
+		//	To handle the backspace pressed
+		if(c=='\b')
+			continue;
+		
+		//	Stores the character read
+		string[i++]=c;
+	}
+	
+	//	Number of characters read
+	return i;					
+}
+
+
+
+int main()
+{
+	char line[MAX];
+	char ch='\0';
+	int i=0;
+	
+	printf("\nEnter the string = ");
+	i=readLine(line);
+	printf("\nLength is %d",i);
+	printf("\n The string is = ");
+	puts(line);
+	
+	/*show_char_array(line);
+	printf("\nUpper String is ");
+	puts(upper(line));
+	printf("\nLower String is ");
+	puts(lower(line));
+	*/
+	return 0;
+}
