f7412359546ff9a51f21f1061a2b7153c1ac8ce8
Debdeep Goswami <53962338+Debdeep-Goswami@users.noreply.github.com>
2020-11-04 20:01:32 +0530

    Add files via upload

--- DevString.c
+++ DevString.c
@@ -13,7 +13,7 @@ int readLine(char *string)
 {	
 
 	/***************************************************************
-		Use		:-	To reads a complete line from stdin/console
+		Use			:-	To reads a complete line from stdin/console
 		
 		Arguments	:-	The char array where the line will be stored
 		
@@ -51,7 +51,30 @@ int readLine(char *string)
 	return i;					
 }
 
-
+void show_char_array(char *line)
+{
+	/***************************************************************
+		Use			:-	To display the charaters inside a string
+		
+		Arguments	:-	The char array to be displayed
+		
+		Return 		:-	void
+		
+	***************************************************************/
+	
+	//	Counter for the character displayed
+	int i=0;
+	
+	printf("\n");
+	
+	//	Loop runs untill null character is read
+	while(line[i])	
+	{
+		printf("\n[ %d ] = %c",i,line[i]);
+		i++;
+	}
+	printf("\n");
+}
 
 int main()
 {
@@ -65,8 +88,8 @@ int main()
 	printf("\n The string is = ");
 	puts(line);
 	
-	/*show_char_array(line);
-	printf("\nUpper String is ");
+	show_char_array(line);
+	/*printf("\nUpper String is ");
 	puts(upper(line));
 	printf("\nLower String is ");
 	puts(lower(line));
